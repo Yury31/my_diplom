f4e33915e01a3b14a5a6e6b4e2c7896e02cd0375
Yury31 <ura3190@gmail.com>
2020-05-21 18:25:33 +0300

    init

---
+++ rednit/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+GENDER_CHOICE = [
+    ['male', u"Мужской"],
+    ['female', u"Женский"],
+]
+
+RELATIONSHIP_CHOICE = [
+    ['none', u"Не определенно"],
+    ['single', u"Холост"],
+    ['in_a_rel', u"В отношениях"],
+    ['engaged', u"Помолвлен(а)"],
+    ['married', u"Женат/Замужем"],
+    ['in_love', u"Влюблен(а)"],
+    ['complicated', u"Все сложно"],
+]
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE,
+                                verbose_name=u"Пользователь")
+    avatar = models.FileField(verbose_name=u"Аватар",
+                              null=True, blank=True)
+    bio = models.TextField(max_length=500,
+                           blank=True,
+                           null=True,
+                           verbose_name=u"О себе")
+    city = models.CharField(max_length=30,
+                            blank=True, null=True,
+                            verbose_name=u"Город")
+    birth_date = models.DateField(null=True,
+                                  blank=True,
+                                  verbose_name=u"Дата рождения")
+    gender = models.CharField(max_length=10,
+                              verbose_name=u"Пол",
+                              choices=GENDER_CHOICE,
+                              default="male")
+    relationship = models.CharField(max_length=20,
+                                    verbose_name=u"Статус отношений",
+                                    choices=RELATIONSHIP_CHOICE,
+                                    default="none")
+# Create your models here.
